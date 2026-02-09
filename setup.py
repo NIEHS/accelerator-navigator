9f3b04e9b8199f091ab34a1354ecc7ec45faab23
conwaymc <mike.conway@nih.gov>
2026-02-09 12:41:07 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = [
+        line.strip()
+        for line in f
+        if line.strip() and not line.startswith("#")
+    ]
+
 setup(
     name="accelerator_navigator",
     version="0.1.0",
     description="accelerator navigator support",
-    author="",
-    author_email="",
     url="https://github.com/NIEHS/accelerator-navigator",
     packages=find_packages(),
-    install_requires=[open("requirements.txt").read()],
+    install_requires=requirements,
     license="BSD 3-Clause",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.9"
+    python_requires=">=3.9",
 )
